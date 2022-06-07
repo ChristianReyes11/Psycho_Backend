06f9fc3231d3c091eae3915fb37c83793fd3d785
Aldo TR <76541277+AldoTR@users.noreply.github.com>
2022-06-07 17:29:26 -0500

    Update tipo_personaRoute.js

--- api/routes/tipo_personaRoute.js
+++ api/routes/tipo_personaRoute.js
@@ -1,1 +1,10 @@
+const { check } = require("express-Router");
+const handleResults = require("../handlers/handleRouter");
 
+const tipo_personaRoutes = [
+   check("descripcion").exists().notEmpty().withMessage("descripcion Invalid").isString().isLength({ min: 6, max: 18 }),
+   check("id_permiso").exists().notEmpty().withMessage("id_permiso Invalid").isNumeric,
+   handleResults,
+];
+
+module.exports = {tipo_personaRouter}
