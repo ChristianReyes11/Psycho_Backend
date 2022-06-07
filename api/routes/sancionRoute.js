4835faa5a9747d6eba4ebf2a20c27bf7b28bfbf7
Aldo TR <76541277+AldoTR@users.noreply.github.com>
2022-06-07 17:36:18 -0500

    Update sancionRoute.js

--- api/routes/sancionRoute.js
+++ api/routes/sancionRoute.js
@@ -1,1 +1,10 @@
+const { check } = require("express-Router");
+const handleResults = require("../handlers/handleRouter");
 
+const sancionRouter = [
+   check("tipo").exists().notEmpty().withMessage("region Invalid").isString().isLength({ min: 6, max: 18 }),
+   check("fecha").exists().notEmpty().withMessage("edad Invalid").isDate(),
+   handleResults,
+];
+
+module.exports = {sancionRouter}
