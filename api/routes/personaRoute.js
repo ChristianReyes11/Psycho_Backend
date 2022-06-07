7d7e481d65765e8254ae05d86e6a615b6d806b43
Aldo TR <76541277+AldoTR@users.noreply.github.com>
2022-06-07 17:54:28 -0500

    Update personaRoute.js

--- api/routes/personaRoute.js
+++ api/routes/personaRoute.js
@@ -1,1 +1,8 @@
+const {personaValidator} = require("../validators/personaValidator")
+const { registerpersona } = require("../controllers/personaController")
+const express = require("express");
+const router = express.Router()
 
+router.post("/register", personaValidator, registerpersona);
+
+module.exports = router;
