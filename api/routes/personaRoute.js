8245b077e060d13e91e54b10550c04abe0cf90c3
Aldo TR <76541277+AldoTR@users.noreply.github.com>
2022-06-07 18:24:31 -0500

    Delete personaRoute.js

--- api/routes/personaRoute.js
+++
@@ -1,8 +0,0 @@
-const {personaValidator} = require("../validators/personaValidator")
-const { registerpersona } = require("../controllers/personaController")
-const express = require("express");
-const router = express.Router()
-
-router.post("/register", personaValidator, registerpersona);
-
-module.exports = router;
