11b576b4bda92340e1446d50aec9681dcab312b1
Ramón AP Quintana <atdiel.perez9709@gmail.com>
2022-06-13 00:24:53 -0500

    using a database from firebase realtime

--- api/controllers/personaController.js
+++ api/controllers/personaController.js
@@ -1,30 +1,58 @@
 const { matchedData } = require("express-validator");
 const { handleHttpError } = require("../handlers/handleError");
-
-const persona = require("../../models/nosql/persona");
-const { response } = require("express");
-
-const db = require("../../config/firebase")
+const { db } = require("../../config/firebase");
 
 /**
  * Aquí registramos a una persona
- * 
- * @param {*} req 
- * @param {*} res 
+ *
+ * @param {*} req
+ * @param {*} res
  */
 const registerPersona = async (req, res) => {
+  try {
     //console.log(req.body); //body todo lleno de ruido ¡innecesario!
-    req = matchedData(req)
-    const newPerson = {...req}
-    const resfire = await db.ref("persona").push(newPerson)
-    console.log(req)
-    res.send({mssg: resfire})
-}
+    req = matchedData(req);
+    const newPerson = { ...req };
+    var data = {};
+    data = await db
+      .ref("persona")
+      .get()
+      .then((personas) => {
+        return personas;
+      });
+    const emailAlreadyExist = data.forEach((persona) => {
+      const p = persona.val();
+      if (p.email === newPerson.email) {
+        return true;
+      }
+    });
+
+    if (emailAlreadyExist) {
+      res.status(409).send({ success: false, mssg: "email already exist!" });
+    } else {
+      // > Registramos a el usuario
+      db.ref("persona").push(newPerson);
+      res.status(200).send({ success: true });
+    }
+
+    // //> Respondemos al usuario con objeto de usuario creado
+    // db.ref("persona").once("value", function (snapshot) {
+    //   return snapshot.forEach(function (childSnapshot) {
+    //     var childData = childSnapshot.val();
+    //     if (childData.email === newPerson.email) {
+    //       res.status(200).send({ success: true, ...childData });
+    //     }
+    //   });
+    // });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 /**
  * Este controlador es el encargado de logear a una persona
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  */
 /* const loginPersona = (req, res) => {
     req = matchedData(req)
@@ -33,24 +61,24 @@ const registerPersona = async (req, res) => {
 }*/
 const loginPersona = async (req, res) => {
   try {
-    req = matchedData(req)
-    console.log(req)
+    req = matchedData(req);
+    console.log(req);
     const userData = matchedData(req);
     //servicio para autenticar usuario
     const data = await authService.userSignIn(userData);
 
     res.send({ data });
   } catch (err) {
-    req = matchedData(req)
-    console.log(req)
+    req = matchedData(req);
+    console.log(req);
     handleHttpError(res, "ERROR_LOGIN", err);
   }
 };
 
 /**
  * Este controlador es el encargado de Actualizar los datos de una persona
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  */
 /*const updatePersona = async (req, res) => {
   try {
@@ -66,5 +94,4 @@ const loginPersona = async (req, res) => {
 };
 */
 
-
-module.exports = {registerPersona, loginPersona, /*updatePersona*/ }
+module.exports = { registerPersona, loginPersona /*updatePersona*/ };
--- config/firebase.js
+++ config/firebase.js
@@ -1,23 +1,11 @@
-const admin = require("firebase-admin");
-
+const firebase = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://psycho-7c867-default-rtdb.firebaseio.com"
-// })
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://psycho-7c867.firebaseio.com",
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://psycho-7c867-default-rtdb.firebaseio.com",
 });
 
-// const db = admin.database();
+const db = firebase.database();
 
-const db = admin.firestore();
-
-module.exports = { db, admin };
-
-//api/foro/ansiedad
-//api/foro/depresion
-//Tocken
+module.exports = { db };
